405b97dac3651a4e675d27de2263c14fe4684ba8
knuklz <a.monaghan90@gmail.com>
2022-08-07 12:21:23 +1000

    Project Setup
    Setting up project and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Raycaster
+# Learning to create a Raycaster
+
+# Resources
+- The Coding Train
+- P5js
+- VSCodium
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(1200,600);
+}
+
+function draw() {
+  
+  background(220);
+  
+}
