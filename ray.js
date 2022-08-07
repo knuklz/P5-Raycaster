342d07f98915dbcafe43d8d33b96a66aeac1ea85
knuklz <a.monaghan90@gmail.com>
2022-08-07 16:40:00 +1000

    Basic line detection setup

---
+++ ray.js
@@ -0,0 +1,51 @@
+class Ray {
+    constructor(x1, y1){
+        this.pos = createVector(x1, y1);
+        this.dir = createVector(1, 0);
+    }
+    show(){
+        stroke(255);
+
+        // Save default origin point
+        push();
+        
+        // Move origin point to current position                       
+        translate(this.pos.x,this.pos.y); 
+        
+        // Draw a line from pos to target
+        line(0, 0, this.dir.x * 10, this.dir.y * 10);
+        
+        // Return origin point to location saved prior
+        pop();
+    }
+    cast(wall){
+        // Describing coordinates of the wall
+        const x1 = wall.a.x;
+        const y1 = wall.a.y;
+        const x2 = wall.b.x;
+        const y2 = wall.b.y;
+
+        // Describing coordinates of the player
+        const x3 = this.pos.x;
+        const y3 = this.pos.y;
+        const x4 = this.pos.x + this.dir.x;
+        const y4 = this.pos.y + this.dir.y;
+
+        const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        // console.log(den);
+        if(den == 0) {
+            return;
+        }
+
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        const u = -((x1 - x2) * (y1 - y2) - (y1 - y3) * (x1 - x3)) / den;
+
+        if(t > 0 && t < 1 && u > 0){
+            return true;
+        } else {
+            return;
+        }
+
+
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+
+let wall;
+let ray;
+
 function setup() {
-  createCanvas(1200,600);
+  createCanvas(800 ,400);
+  wall = new Boundary(300, 100, 300, 300);
+  ray = new Ray(100,200);
 }
 
 function draw() {
+  background(1);
+  wall.show();
+  ray.show();
+
+  let pt = ray.cast(wall);
+  console.log(pt);  
   
-  background(220);
-  
+  // if(pt) {
+  //   stroke(255);
+  //   ellipse(pt.x, pt.y, 8, 8);
+  // }
+
+
+
 }
